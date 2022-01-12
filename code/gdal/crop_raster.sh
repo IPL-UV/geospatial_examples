01185393c9de20e39329d7e1cc6bf8e77ca3fcdc
gherardo varando <gherardo.varando@gmail.com>
2022-01-12 09:50:13 +0100

    crop raster gdal example

---
+++ code/gdal/crop_raster.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# crop a raster using gdal 
+
+# define source and destination files
+SRCFILE="data/wc2.1_10m_tavg/wc2.1_10m_tavg_01.tif"
+DSTFILE="cropped_raster.tif"
+
+# with the -projwin option we can specify 
+# upper-left and lower-right coordinates 
+# of the cropped region in georeferenced coordinates
+# in this case UL = (-1, 40), LR = (0, 39)
+gdal_translate -projwin -1 40 0 39 ${SRCFILE} ${DSTFILE} 
+
+
