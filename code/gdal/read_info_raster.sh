7c2272d8da0ee79090e4451238e91bcc60fdaa31
gherardo varando <gherardo.varando@gmail.com>
2022-01-12 10:02:48 +0100

    upd comments with author

--- code/gdal/read_info_raster.sh
+++ code/gdal/read_info_raster.sh
@@ -2,5 +2,6 @@
 #
 # use the gdalinfo program to read geospatial 
 # info from the raster file
+# author: Gherardo Varando (github: gherardovarando)
 
 gdalinfo data/wc2.1_10m_tavg/wc2.1_10m_tavg_01.tif 
--- code/gdal/read_info_vector.sh
+++ code/gdal/read_info_vector.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+#
+# use ogrinfo to read geospatial info 
+# from a vector file
+# author: Gherardo Varando (github: gherardovarando) 
 
-## use the ogrinfo to open the shapefile
-## you can use both the .shp (.shx) or the folder path 
+# use the ogrinfo to open the shapefile
+# we can use both the .shp (.shx) or the folder path 
 ogrinfo data/ne_110m_admin_0_countries
